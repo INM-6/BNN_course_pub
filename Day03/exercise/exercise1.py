4b8f9d8958c300b11407ef2b47e578151bab7506
rpauli <r.pauli@fz-juelich.de>
2017-07-18 22:51:59 +0200

    added exercise and lecture day 3

---
+++ Day03/exercise/exercise1.py
@@ -0,0 +1,185 @@
+﻿import numpy as np
+import pylab
+import nest
+
+"""
+Setting up NEST
+"""
+nest.ResetKernel()
+np.random.seed()
+rndSeeds = np.random.randint(0, 1000, 2)
+rngSeeds = [rndSeeds[0]]
+grngSeed = rndSeeds[1]
+dt = .1
+nest.SetStatus([0],{"resolution"        : dt,
+                    "rng_seeds"         : rngSeeds,
+                    "grng_seed"         : grngSeed})
+
+"""
+Simulation parameters
+"""
+simtime = 10000.
+wuptime = 400.
+
+"""
+Network parameters
+"""
+mode        = "both"
+ni          = 1000 #number of neurons
+epsilon_ii  = .2 # connection probability
+c_ii        = int(epsilon_ii * ni) # number of pre-post synaptic neurons
+ri          = 100#number of neurons to record from
+je          = 1. #weight of external input
+g           = 10  #strength ratio inh-exc
+ji          = -je * g # inhibitory weights
+delay_ii    = 2.
+delay_ext    = 1.
+
+"""
+Neuron parameters
+"""
+neuron_model= "iaf_psc_alpha"
+tauSynEx    = .5
+tauSynIn    = .5
+tauMem      = 20.
+theta       = 20.
+reset       = .0
+neuron_params= {"C_m"       : 1.,
+                "tau_m"     : tauMem,
+                "tau_syn_ex": tauSynEx,
+                "tau_syn_in": tauSynIn,
+                "t_ref"     : 2.,
+                "E_L"       : .0,
+                "V_m"       : .0,
+                "V_reset"   : reset,
+                "V_th"      : theta}
+
+"""
+External input
+"""
+ext = nest.Create("dc_generator", params={"amplitude":13.})
+
+"""
+Recorders
+"""
+recorder_params = {"start"        : wuptime,
+                   "to_file"     : False,
+                   "to_memory"   : True}
+sd_i = nest.Create("spike_detector", params=recorder_params)
+vm_i = nest.Create("voltmeter", params=recorder_params)
+
+"""
+Create neurons
+"""
+inh = nest.Create(neuron_model, ni, params=neuron_params)
+
+"""
+Connect neurons
+
+Write your code here
+--------------------
+Implement different connectivity schemes as indicated in 
+the exercise sheet. Start with fixing the out-degree and then do the same 
+with the in-degree. Finally just make both broader. Write your code such that 
+you can switch easily from one option to the other. Look in the exercise sheet 
+for hints if you need.
+"""
+
+
+
+"""
+Connect external drive
+"""
+nest.Connect(ext, inh, "all_to_all",
+             syn_spec={"weight":je, "delay":delay_ext})
+
+"""
+Connect to recorders
+"""
+nest.Connect(inh, sd_i, "all_to_all",
+             syn_spec={"weight":1.0, "delay":1.0})
+
+nest.Connect(vm_i, inh[:ri], "all_to_all",
+             syn_spec={"weight":1.0, "delay":1.0})
+                      
+
+
+"""
+Simulate
+"""
+nest.Simulate(wuptime + simtime)
+
+"""
+Get data from recorders
+ti: a list of all firing times
+si: a list of the corresponding neuron ids
+pvi: a list of all recorded membrane potentials
+tvi: a list of the corresponding recording times
+svi: a list of the corresponding neuron ids
+"""
+data_si = nest.GetStatus(sd_i, "events")[0]
+si, ti = data_si["senders"], data_si["times"]
+
+data_vi = nest.GetStatus(vm_i, "events")[0]
+svi, tvi, pvi = np.array(data_vi["senders"]), np.array(data_vi["times"]), np.array(data_vi["V_m"])
+
+
+"""
+Plot membrane potential trace (recorder neurons gray + average in blue)
+
+Write your code here
+--------------------
+Write a function or a just a piece of code that plots the membrane traces of the
+recorded neurons in gray. Calculate the average and plot it in a different color.
+Please note that svi,tvi,pvi have been already converted into numpy arrays.
+
+"""
+pylab.figure()
+
+
+pylab.xlabel("time(ms)", fontsize=30)
+pylab.ylabel("potential(mV)", fontsize=30)
+
+
+
+
+
+
+"""
+Plot rate histogram
+
+Write your code here
+--------------------
+
+Calculate the number of spikes emitted by each neuron and store
+results in a rate vector, which you can then apply hist to
+"""
+
+pylab.figure()
+
+
+pylab.xlabel("rate [Hz]", fontsize=30)
+pylab.ylabel("counts", fontsize=30)
+
+
+"""
+Bonus: Plot raster plot ordered by rate
+
+Write your code here
+--------------------
+
+Use the rate vector calculated in the previous exercise to sort the spiking data
+such that neurons with a higher firing rate are plotted above neurons with a
+lower firing rate.
+
+"""
+#Uncomment the following section to do the bonus analysis
+#pylab.figure()
+
+#pylab.xlabel("time(ms)", fontsize=30)
+#pylab.ylabel("neuron id", fontsize=30)
+
+
+
+pylab.show()
+
---
+++ Day03/exercise/exercise3.py
@@ -0,0 +1,230 @@
+﻿import numpy as np
+import random as rand
+import pylab as pl
+import nest
+import nest.topology as topo
+
+"""
+Setting up NEST
+"""
+nest.ResetKernel()
+np.random.seed()
+rndSeeds = np.random.randint(0, 1000, 2)
+rngSeeds = [rndSeeds[0]]
+grngSeed = rndSeeds[1]
+dt = .1
+nest.SetStatus([0], {"resolution"        : dt,
+                     "rng_seeds"         : rngSeeds,
+                     "grng_seed"         : grngSeed})
+
+"""
+Simulation parameters
+"""
+simtime = 10000.
+wuptime = 200.
+
+
+"""
+You may want to use these variables to alternate between the different 
+configurations. You need long simualtions to stimate correlation coefficients
+properly.
+"""
+gauss_ex = False
+gauss_in = False
+
+
+"""
+Network parameters
+"""
+order   = 1000
+ne      = 4 * order
+ni      = 1 * order
+epsilon = .1
+je      = .1
+g       = 10
+ji      = -g * je
+delay   = 1.5
+
+#topology parameters
+extentX = 1.
+extentY = 1.
+wrapped = True
+
+#layer parameters
+layer_dict = {"extent"      : [extentX, extentY],
+              "edge_wrap"   : wrapped}
+
+#connection parameters
+
+"""
+Write your code here
+--------------------
+
+Write the dictionaries that you will use to connect the neurons locally. You can
+use one dictionary for both populations if you wish. Remember that your choice 
+of parameters has to match the description given in the exercise sheet. Number
+of connections have to be roughly the same as in the random case. Use the
+function FindConnections() to find out whether you are making the same number of
+connections or not.
+"""
+
+condict_ex = {}
+
+condict_in = {}
+
+"""
+Neuron parameters
+"""
+neuron_model= "iaf_psc_alpha"
+tauSynEx    = .5
+tauSynIn    = .5
+tauMem      = 20.
+theta       = 20.
+reset       = .0
+neuron_params= {"C_m"       : 1.,
+                "tau_m"     : tauMem,
+                "tau_syn_ex": tauSynEx,
+                "tau_syn_in": tauSynIn,
+                "t_ref"     : 2.,
+                "E_L"       : .0,
+                "V_m"       : .0,
+                "V_reset"   : reset,
+                "V_th"      : theta}
+
+
+nest.CopyModel("iaf_psc_alpha", "exc", params=neuron_params)
+nest.CopyModel("iaf_psc_alpha", "inh", params=neuron_params)
+
+"""
+Part a:
+Create layers
+
+The following code distributes the neurons randomly across the layer.
+"""
+
+ex_pos = [[np.random.uniform(-extentX / 2., extentX / 2.),\
+           np.random.uniform(-extentY / 2., extentY / 2.)] for j in xrange(ne)]
+layer_dict.update({"positions": ex_pos,"elements":"exc"})
+in_pos = [[np.random.uniform(-extentX / 2., extentX / 2.),\
+           np.random.uniform(-extentY / 2., extentY / 2.)] for j in xrange(ni)]
+layer_dict.update({"positions": in_pos, "elements":"inh"})
+
+
+"""
+Write your code here
+--------------------
+Create a layer of inhibitory neurons and another of excitatory neurons. Use
+the funcion topo.CreateLayer() for that.
+"""
+
+
+"""
+Part b:
+Connect the layers
+"""
+
+"""
+Write your code here
+--------------------
+
+Connect the layers (EE,EI,IE,II). Write your code in such a way that you can 
+easily switch between random and local connectivity.
+"""
+
+"""
+External input
+"""
+ext_rate=7.8
+ext = nest.Create("poisson_generator",params={"rate":ext_rate*1e3})
+
+"""
+Recorders
+"""
+recorder_params    ={"start"        : wuptime,
+                      "to_file"     : False,
+                      "to_memory"   : True}
+sd = nest.Create("spike_detector", params=recorder_params)
+vm = nest.Create("voltmeter", params=recorder_params)
+
+"""
+Connect external drive
+"""
+nest.Connect(ext, inhNeurons + excNeurons, "all_to_all",
+             syn_spec={"weight":je, "delay":1.})
+             
+
+"""
+Connect to recorders
+"""
+nest.Connect(excNeurons, sd,"all_to_all",
+             syn_spec={"weight":1.0, "delay":1.0})
+            
+nest.DivergentConnect(vm, excNeurons, "all_to_all",
+             syn_spec={"weight":1.0, "delay":1.0})
+             
+
+
+"""
+Part b cont:
+Plot the network
+"""
+
+"""
+Write your code here
+--------------------
+
+Plot the network connectivity. Re-use the function provided in 
+exercise 2.
+"""
+
+"""
+Simulate
+"""
+nest.Simulate(wuptime + simtime)
+
+"""
+Get data from recorders
+"""
+data_sd = nest.GetStatus(sd, "events")[0]
+s_sd,t_sd = data_sd["senders"], data_sd["times"]
+
+data_v = nest.GetStatus(vm, "events")[0]
+s_v,t_v,p_v = np.array(data_v["senders"]), np.array(data_v["times"]), np.array(data_v["V_m"])
+
+"""
+Part c:
+Analyse the data
+"""
+
+"""
+Write your code here
+--------------------
+
+Plot the spiking activity and the PSTH. Re-use the code that you implemented in 
+previous exercises.
+"""
+
+
+
+def get_ccs(times, senders, n_sample=1000, bin_size=5.):
+    unique_ids = np.unique(senders)
+    bins= np.arange(wuptime, simtime + wuptime + bin_size, bin_size)
+    cc = np.zeros(n_sample)
+    for i in xrange(n_sample):
+        sp1, sp2 = rand.sample(unique_ids, 2)
+        psth1 = np.histogram(times[senders == sp1], bins)[0]
+        psth2 = np.histogram(times[senders == sp2], bins)[0]
+        cc[i] = np.corrcoef(psth1, psth2)[0][1]
+    return cc
+
+
+"""
+Write your code here
+--------------------
+
+Plot the distribution of correlation coefficients. Use the function pl.hist().
+"""
+pl.xlabel("correlation coefficient", fontsize=30)
+pl.ylabel("counts", fontsize=30)
+pl.show()
+
